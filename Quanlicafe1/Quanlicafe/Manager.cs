2810c318ec131849fa4665e60665649f7a5b0f4b
Tranminhhoan06 <65876952+Tranminhhoan06@users.noreply.github.com>
2020-06-01 23:54:50 +0700

    hoàn thành chức năng thêm món

--- Quanlicafe1/Quanlicafe/Manager.cs
+++ Quanlicafe1/Quanlicafe/Manager.cs
@@ -25,7 +25,18 @@ namespace Quanlicafe
                 DataProvider d = new DataProvider();
                 string name = cbCafe.Text;
                 string num = numericUpDown1.Value.ToString();
-                lsvBill.Items.Add(d.them(name, num));
+                int k = 0;
+                if (int.Parse(num) != 0)
+                {
+                    for (int i = 0; i < lsvBill.Items.Count; i++)
+                        if (name == lsvBill.Items[i].SubItems[0].Text)
+                        {
+                            lsvBill.Items[i].SubItems[1].Text = (int.Parse(lsvBill.Items[i].SubItems[1].Text) + int.Parse(num)).ToString();
+                            k = 1;
+                        }
+                    if (k == 0)
+                        lsvBill.Items.Add(d.them(name, num));
+                }
             }
         }
 
